7c340acbdcdb9d1b7128103384a59981d08b4031
Isaac Gomez <angelisasc1968@gmail.com>
2024-06-06 12:58:01 -0600

    Fin dia 1

--- 2023/Day_1/trebuchet/trebuchet.cpp
+++ 2023/Day_1/trebuchet/trebuchet.cpp
@@ -1,19 +1,51 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <sstream>
+#include <unordered_map>
 
 int extractInt(std::string line) {
-    // Extract the firt and last integers from the given string (line)
+    // Extract the firt and last integers from the given string (line) incluiding numbers written (one, two, etc)
 
-    // store the number as string
+    // Empty string for storing the digits
     std::string num = "";
-    // Loop over every character of string
-    for (char& c: line) {
-        if (isdigit(c)) {
-            num += c;
+
+    // Create unordered map that maps number name to digit
+    std::unordered_map<std::string, int> wordDigit = {
+        {"one", 1},
+        {"two", 2},
+        {"three", 3},
+        {"four", 4},
+        {"five", 5},
+        {"six", 6},
+        {"seven", 7},
+        {"eight", 8},
+        {"nine", 9},
+        {"one", 1}
+    };
+
+    for (int i=0; i < line.length(); i++) {
+        // substring with starting in ith char to last char in string
+        std::string substr = line.substr(i);
+
+        // check if first char is digit and add it to num string
+        if (isdigit(substr[0])){
+            num += substr[0];
+        }
+
+        // check if it is a written number if numbers have not already been found
+        else {
+            // Check every written digit
+            for (auto x: wordDigit) {
+                // Check if string starts with written number
+                size_t pos = substr.rfind(x.first, 0);
+                // If written digit is found, map the string to integer and add it to num
+                if (pos != std::string::npos){
+                    num += std::to_string(x.second);
+                }
+            }
         }
     }
+    
     // get first and last digits for number to form a 2 digit number
     char first = num[0];
     char last = num[num.length()-1];
@@ -50,7 +82,6 @@ int main(){
     // Close file
     file.close();
 
-    std::cout << sum << std::endl;
-
+    std::cout << "Calibration Value: " << sum << std::endl;
     return 0;
 }
